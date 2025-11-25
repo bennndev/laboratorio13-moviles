ce9ed3644da1c4500921a9ac894f45d13847aebf
neB <benjamin.sullca1103@gmail.com>
2025-11-25 10:15:43 -0500

    feat: primer ejercicio desarrollado en Ejercicio1Screen.kt

---
+++ app/src/main/java/com/example/laboratorio13_moviles/Ejercicio1Screen.kt
@@ -0,0 +1,61 @@
+package com.example.laboratorio13_moviles
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.laboratorio13_moviles.ui.theme.Laboratorio13movilesTheme
+
+@Composable
+fun Ejercicio1Screen(modifier: Modifier = Modifier) {
+    var isVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Ejercicio 1: Benjamín Sullca",
+            fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+            modifier = Modifier.padding(bottom = 24.dp)
+            .align(Alignment.CenterHorizontally)
+        )
+        Button(onClick = { isVisible = !isVisible }) {
+            Text(if (isVisible) "Ocultar cuadro" else "Mostrar cuadro")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        AnimatedVisibility(
+            visible = isVisible,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(150.dp)
+                    .background(Color(0xFF4CAF50))
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Ejercicio1Preview() {
+    Laboratorio13movilesTheme {
+        Ejercicio1Screen()
+    }
+}
